dcfc6fc848e1a047eb670a388f5b3d0b73453a82
BeniCA1 <136569758+BeniCA1@users.noreply.github.com>
2023-07-10 20:13:21 +0200

    Delete Again

--- Beni/Frontend/src/app/guards/login.guard.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { AuthService } from '../service/auth.service';
-
-export const loginGuard: CanActivateFn = (route, state) => {
-  const auth = inject(AuthService);
-  return(auth.login());
-};
