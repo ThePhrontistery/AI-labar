5351502e98bd4a8ccec5d4f6dda31676b9380e68
BeniGV <benigno.garcia-velasco@capgemini.com>
2023-08-29 14:55:17 +0200

    Swagger configuration updated

--- Backend/ai-labar/src/main/java/com/capgemini/ailabar/commons/configurations/SwaggerConfiguration.java
+++ Backend/ai-labar/src/main/java/com/capgemini/ailabar/commons/configurations/SwaggerConfiguration.java
@@ -15,7 +15,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.capgemini.beni.ailabar"))
+                .apis(RequestHandlerSelectors.basePackage("com.capgemini.ailabar"))
                 .paths(PathSelectors.any())
                 .build();
     }
