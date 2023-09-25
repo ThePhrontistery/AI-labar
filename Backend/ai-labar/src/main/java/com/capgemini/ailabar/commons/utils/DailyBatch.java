a2a9b9b5e04bd2234b43a268db9a702eef9ea5d2
BeniGV <benigno.garcia-velasco@capgemini.com>
2023-09-25 10:42:27 +0200

    Logs control changed for problems

--- Backend/ai-labar/src/main/java/com/capgemini/ailabar/commons/utils/DailyBatch.java
+++ Backend/ai-labar/src/main/java/com/capgemini/ailabar/commons/utils/DailyBatch.java
@@ -1,25 +1,25 @@
 package com.capgemini.ailabar.commons.utils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DailyBatch {
-    private static final Logger logger = LoggerFactory.getLogger(DailyBatch.class);
+    private static final Logger logger = LogManager.getLogger(DailyBatch.class);
     private final DailyBatchService dailyBatchService;
 
     @Autowired
     public DailyBatch(DailyBatchService dailyBatchService) {
-        logger.debug("Start batch");
+        logger.info("Start batch");
         this.dailyBatchService = dailyBatchService;
     }
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void runDailyBatch() {
-        logger.debug("Enter on runDailyBatch");
+        logger.info("Enter on runDailyBatch");
         dailyBatchService.closeExpiredTopics();
     }
 }
